1c2b825d99daa20bfdd5693d88c14976206f4f51
Lao·Zhu <2208213223@qq.com>
2021-01-28 23:38:42 +0800

    pwm generate code debug, it can generate pwm now

--- Program/Hardware/tim.c
+++ Program/Hardware/tim.c
@@ -10,27 +10,24 @@ void TIM_SetDutyCycle(float DutyCycle, unsigned int Channel) {
             break;
         case 1:TIMER_CH1CV(TIMER0) = (uint32_t) ((Period * DutyCycle) / 1);
             break;
-        case 2:TIMER_CH2CV(TIMER0) = (uint32_t) ((Period * DutyCycle) / 1);
-            break;
         default:break;
     }
 }
 
 void TIM_Config(void) {
-    timer_oc_parameter_struct timer_ocintpara;
-    timer_parameter_struct timer_initpara;
-
     Period = (54000 / PWM_FREQUENCY) - 1;
-
-    rcu_periph_clock_enable(RCU_TIMER0);
-    rcu_periph_clock_enable(RCU_TIMER1);
     rcu_periph_clock_enable(RCU_GPIOA);
     rcu_periph_clock_enable(RCU_AF);
+
     gpio_init(GPIOA, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_8);
     gpio_init(GPIOA, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_9);
-    gpio_init(GPIOA, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_10);
 
+    timer_oc_parameter_struct timer_ocintpara;
+    timer_parameter_struct timer_initpara;
+    rcu_periph_clock_enable(RCU_TIMER0);
     timer_deinit(TIMER0);
+
+    /* TIMER0 configuration */
     timer_initpara.prescaler = 1;
     timer_initpara.alignedmode = TIMER_COUNTER_EDGE;
     timer_initpara.counterdirection = TIMER_COUNTER_UP;
@@ -39,32 +36,27 @@ void TIM_Config(void) {
     timer_initpara.repetitioncounter = 0;
     timer_init(TIMER0, &timer_initpara);
 
-    timer_ocintpara.ocpolarity = TIMER_OC_POLARITY_HIGH;
     timer_ocintpara.outputstate = TIMER_CCX_ENABLE;
+    timer_ocintpara.outputnstate = TIMER_CCXN_ENABLE;
+    timer_ocintpara.ocpolarity = TIMER_OC_POLARITY_HIGH;
     timer_ocintpara.ocnpolarity = TIMER_OCN_POLARITY_HIGH;
-    timer_ocintpara.outputnstate = TIMER_CCXN_DISABLE;
     timer_ocintpara.ocidlestate = TIMER_OC_IDLE_STATE_LOW;
     timer_ocintpara.ocnidlestate = TIMER_OCN_IDLE_STATE_LOW;
 
+    timer_channel_output_config(TIMER0, TIMER_CH_0, &timer_ocintpara);
     timer_channel_output_config(TIMER0, TIMER_CH_1, &timer_ocintpara);
-    timer_channel_output_config(TIMER0, TIMER_CH_2, &timer_ocintpara);
-    timer_channel_output_config(TIMER0, TIMER_CH_3, &timer_ocintpara);
 
-    timer_channel_output_pulse_value_config(TIMER0,
-                                            TIMER_CH_1,
-                                            (unsigned short) ((Period + 1) * PWM_INITDUTY / 100) - 1);
+    timer_channel_output_pulse_value_config(TIMER0, TIMER_CH_0,
+                                            (unsigned short) ((Period + 1) * PWM_INITDUTY) - 1);
+    timer_channel_output_mode_config(TIMER0, TIMER_CH_0, TIMER_OC_MODE_PWM0);
+    timer_channel_output_shadow_config(TIMER0, TIMER_CH_0, TIMER_OC_SHADOW_DISABLE);
+
+    timer_channel_output_pulse_value_config(TIMER0, TIMER_CH_1,
+                                            (unsigned short) ((Period + 1) * PWM_INITDUTY) - 1);
     timer_channel_output_mode_config(TIMER0, TIMER_CH_1, TIMER_OC_MODE_PWM0);
     timer_channel_output_shadow_config(TIMER0, TIMER_CH_1, TIMER_OC_SHADOW_DISABLE);
-    timer_channel_output_pulse_value_config(TIMER0,
-                                            TIMER_CH_2,
-                                            (unsigned short) ((Period + 1) * PWM_INITDUTY / 100) - 1);
-    timer_channel_output_mode_config(TIMER0, TIMER_CH_2, TIMER_OC_MODE_PWM0);
-    timer_channel_output_shadow_config(TIMER0, TIMER_CH_2, TIMER_OC_SHADOW_DISABLE);
-    timer_channel_output_pulse_value_config(TIMER0,
-                                            TIMER_CH_3,
-                                            (unsigned short) ((Period + 1) * PWM_INITDUTY / 100) - 1);
-    timer_channel_output_mode_config(TIMER0, TIMER_CH_3, TIMER_OC_MODE_PWM0);
-    timer_channel_output_shadow_config(TIMER0, TIMER_CH_3, TIMER_OC_SHADOW_DISABLE);
+
+    timer_primary_output_config(TIMER0, ENABLE);
 
     timer_auto_reload_shadow_enable(TIMER0);
     timer_enable(TIMER0);
--- Program/User/main.c
+++ Program/User/main.c
@@ -1,9 +1,9 @@
 #include "main.h"
+
 pass_parameters a;
 float Amplitude_Frequency_Curve[128];
-//float  sin_tab[128];
 void Butterworth_Test(void) {
-    a.N = 9;                        //改变butterworth阶数
+    a.N = 4;
     float as[a.N + 1], bs[a.N + 1], az[a.N + 1], bz[a.N + 1],
         data_buffer[a.N + 1];
     for (unsigned char counter = 0; counter <= a.N; counter++) {
@@ -19,10 +19,8 @@ void Butterworth_Test(void) {
         float tmp_buf[0xff];
         float theta = 0;
         for (unsigned char gen_cnt = 0; gen_cnt < 0xff; gen_cnt++) {
-            theta = theta + 0.0246399f * freq;
-            a.dDataIn = XSin(theta);
-//            Curve[1]=a.dDataIn;
-//            Delayms(5);
+            theta = theta + 0.0246399f * (float) freq;
+            a.dDataIn = (float) XSin(theta);
             tmp_buf[gen_cnt] = butterworth_calculate(&a);
         }
         Amplitude_Frequency_Curve[freq] = user_max(tmp_buf, 0xff);
@@ -38,61 +36,15 @@ int main(void) {
     DMA_Config();
     ADC_Config();
     TIM_Config();
+    TIM_SetDutyCycle(0.001f, 0);
+    TIM_SetDutyCycle(0.1f, 1);
     Butterworth_Test();
     for (int counter = 0; counter < 128; ++counter) {
         Curve[0] = Amplitude_Frequency_Curve[counter];
-        if(counter==127)
-        {
-            counter=0;
+        if (counter == 127) {
+            counter = 0;
         }
         Delayms(50);
     }
     while (1);
 }
-
-//int main(void)            //pid 调试
-//{
-//    System_Config();
-//    CAN_Config();
-//    GPIO_Config();
-//    UART_Config();
-//    SPI_Config();
-//    DMA_Config();
-//    ADC_Config();
-//    TIM_Config();
-//    pid.goal_value=30;
-//    pid.actual_value=0;
-//    pid.output_max=2000;
-//    pid.output_min=-2000;
-//    pid.Integral_max=2000;
-//    pid.goal_value=0;
-//    int ii=0;
-//    int flag=1;
-//    Parameters[0]=Parameters[1]=Parameters[2]=0;
-//    Parameters[3]=0;
-//    Curve[3]=0;
-//        while(1)
-//        {
-//            pid.kp=Parameters[0];
-//            pid.ki=Parameters[1];
-//            pid.kd=Parameters[2];
-//            filter.low_pass_parameter=Parameters[3];
-//
-//            if(ii++==200)
-//            {
-//                ii=0;
-//                if(flag==1){
-//                    pid.goal_value+=1000;
-//                    flag=0;
-//                } else{
-//                    pid.goal_value-=1000;
-//                    flag=1;
-//                }
-//            }
-//            pid.actual_value+=pid_calculate(&pid);
-//            Curve[0]=pid.goal_value;
-//            Curve[1]=pid.actual_value;
-//            Delayms(50);
-//        }
-//}
-
